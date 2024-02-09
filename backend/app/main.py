695f14a6a5d78e4bbbf26a7caa0436cc1530e1c1
orestisathanasopoulos <okcyborg@gmail.com>
2024-02-09 08:31:50 +0100

    compose update

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,10 @@ app =  FastAPI(
 origins = [
     "http://localhost:4173",
     "http://localhost:8080",
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "http://host.docker.internal:4173",
+    "http://host.docker.internal:5173",
+    
 ]
 
 app.add_middleware(
